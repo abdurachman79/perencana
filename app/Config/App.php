6568b5240b14bee89609609ae3763c026952cbfb
abdurachman <abdurachman79@gmail.com>
2022-08-05 09:27:17 +0700

    commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
 	 * @var string
 	 */
 	// public $baseURL = 'http://localhost:8080/perencana/';
-	public $baseURL = 'http://localhost/perencana/';
+	public $baseURL = 'http://localhost:8080/perencana/';
 
 	/**
 	 * --------------------------------------------------------------------------
